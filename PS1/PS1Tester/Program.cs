5e908d21f7a9636614f3acd127c421e9632f08f1
kkossin <kmkossin@gmail.com>
2015-09-07 14:29:33 -0600

    Still need lookup and regex fix

---
+++ PS1/PS1Tester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS1Tester
+{
+    class Program
+    {
+        public static int Test(string[] args)
+        {
+            string test1 = "(3+3)";
+            string test2 = "(3-3)";
+            string test3 = "(3*3)";
+            string test4 = "(3/3)";
+
+            FormulaEvaluator.
+        }
+    }
+}
